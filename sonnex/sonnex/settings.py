669c985c793f808bc7005f72053b9f5671c80452
SimpleNick <simplenick01@gmail.com>
2022-11-07 17:44:18 +0100

    Modified Settings: Debug & Secret Key

--- sonnex/sonnex/settings.py
+++ sonnex/sonnex/settings.py
@@ -1,19 +1,21 @@
 
-
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&l@rygh1&pc3dgxn&humwbksse8fui)cm_c0nqsqzxo6*zn2(0"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
